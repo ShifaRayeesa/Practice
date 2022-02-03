3073ef8411173e139d916069a010df28032e6cc9
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-02-03 15:56:38 +0530

    Create 5-ByteShort.java

---
+++ 5-ByteShort.java
@@ -0,0 +1,8 @@
+public class ByteShort {
+    public static void main(String[] args)
+    {
+        short shortNumber=150;
+        byte byteNumber=(byte) shortNumber;
+        System.out.println(byteNumber);
+    }
+}
