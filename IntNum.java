7abcc80c71ae30f7181962ed527e766d5a931b48
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-02-03 13:22:19 +0530

    Add files via upload

---
+++ IntNum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class IntNum
+{
+    public static void main(String[] args)
+    {
+        float floatNumber = 150.9f;
+        int intNumber =(int) floatNumber;
+        System.out.println(intNumber);
+    }
+}
