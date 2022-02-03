e0f7f50f9c3b124103f30104963fc69f69b61232
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-02-03 16:16:35 +0530

    Update and rename 4-Datatype.java to 004-Datatype.java

---
+++ 004-Datatype.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+/*public class Datatype
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+        String s2= sc.next();
+        float x = Float.parseFloat(s1);
+        double y = Double.parseDouble(s2);
+        if(x==y)
+        {  System.out.println("equal"); }
+        else
+        {  System.out.println("not equal"); }
+    }
+}*/
+
+public class Datatype
+{
+    public static void main(String args[])
+    {
+        float x = 6.02f;
+        double y = 6.02d;
+        if(x==y)
+        {  System.out.println("equal"); }
+        else
+        {  System.out.println("not equal"); }
+    }
+}
