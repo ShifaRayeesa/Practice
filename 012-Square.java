a8c1847c15b82ae01ba7c8de1999011df8b57079
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-02-03 19:08:50 +0530

    Create 012-Square.java

---
+++ 012-Square.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Square {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        float side=sc.nextFloat();
+        System.out.println(side*side);
+    }
+}
