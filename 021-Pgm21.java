cff0c7fa7cb98c28df8d1abc9c2e9d9e8649763e
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-02-03 20:44:37 +0530

    Create 021-Pgm21.java

---
+++ 021-Pgm21.java
@@ -0,0 +1,14 @@
+public class Pgm21 {
+    public static void main(String[] args)
+    {
+        int a=5,b=7,x;
+        System.out.println(a*=b+5);   //add then *=
+        x=++a - b--;
+        System.out.println(x);
+        System.out.println(a+" "+b);
+        a=7;b=5;
+        System.out.println(a%b++);
+        x=69>>>2;//1000101 => 10001=17
+        System.out.println(x);
+    }
+}
