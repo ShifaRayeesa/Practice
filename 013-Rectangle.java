3c301209a427168075c01d1c6dcde2f7836696ee
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-02-03 19:09:53 +0530

    Create 013-Rectangle.java

---
+++ 013-Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        float len=sc.nextInt();
+        float breadth=sc.nextInt();
+        System.out.println(len*breadth);
+    }
+
+}
