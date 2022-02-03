e4a07fb42706e21826aa992780192787da873d0c
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-02-03 16:02:04 +0530

    Create 9-PrintTypes.java

---
+++ 9-PrintTypes.java
@@ -0,0 +1,8 @@
+public class PrintTypes {
+    public static void main(String arg[]) {
+        System.out.println(7+7);
+        System.out.println(7+7.0);
+        System.out.println(7+'7');
+    }
+
+}
