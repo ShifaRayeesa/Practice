4e8bfd796008bf620a400bb64780a8b81fbf540e
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-02-03 13:21:12 +0530

    Add files via upload

---
+++ TrueFalse.java
@@ -0,0 +1,10 @@
+public class TrueFalse
+{
+    public static void main(String[] args)
+    {
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(true&true);
+        System.out.println(true&false);
+    }
+}
