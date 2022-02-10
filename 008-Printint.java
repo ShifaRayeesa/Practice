ba3c346e82c7732c3042b1c423d194049c67453c
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-02-10 15:18:35 +0530

    Update 008-Printint.java

--- 008-Printint.java
+++ 008-Printint.java
@@ -1,11 +1,10 @@
 
-
-public class Printint
+public class IntChar
 {
     public static void main(String[] args)
     {
         int number55 = 55;
         char character55 = (char) number55;
-        System.out.println(number55);
+        System.out.println(character55);
     }
 }
